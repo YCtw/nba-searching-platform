9a32d9e138ee96f9cfa878f9785b073b8f2d036f
John Chen <yuhanschen@gmail.com>
2024-12-26 21:09:37 +0800

    Adjusting the form of API request due to the adjustment on the API end

--- main.py
+++ main.py
@@ -3,20 +3,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
-import requests
-import os
 from teamlogo import team_logo_dict
+from balldontlie import BalldontlieAPI
+import os
 
 #Information of data resource
-PLAYER_STATS_SEASON = 2023
-PLAYER_ID_GET_URL = "https://api.balldontlie.io/v1/players"
-PLAYER_STATS_URL = "https://api.balldontlie.io/v1/stats"
-PLAYER_IMAGE_URL = f"http://data.nba.net/data/10s/prod/v1/{PLAYER_STATS_SEASON}/players.json"
+API_KEY = os.environ.get("api_key")
+api = BalldontlieAPI(api_key=API_KEY)
 UNKNOWN_IMAGE_URL = "https://i.ibb.co/yh2KG8P/unknown.jpg"
-TEAM_URL = "http://api.balldontlie.io/v1/teams"
-headers = {
-    "Authorization":os.getenv('HEADER_CREDENTIAL')
-}
 
 #Basic setup
 app = Flask(__name__)
@@ -36,43 +30,27 @@ class TeamForm(FlaskForm):
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     player_form = PlayerForm()
+
+    #Post
     if player_form.validate_on_submit() == True:
-        first_name = player_form.first_name.data
-        last_name = player_form.last_name.data
+        first_name = player_form.first_name.data.strip()
+        last_name = player_form.last_name.data.strip()
 
-        # Call the player information API to get the ID and position
-        PLAYER_PARAMS = {
-            "first_name":first_name.strip(),
-            "last_name":last_name.strip()
-        }
+        #Call the API to get response
         try:
-            PLAYER_INFO_RESPONSE = requests.get(url=PLAYER_ID_GET_URL, headers=headers, params=PLAYER_PARAMS)
-            PLAYER_INFO_DATA = PLAYER_INFO_RESPONSE.json()
-
+            players_response = api.nba.players.list(first_name=first_name, last_name=last_name)
+            PLAYER_INFO_DATA = players_response.data
             #If found this player
-            if len(PLAYER_INFO_DATA["data"]) > 0:
-                PLAYER_ID = PLAYER_INFO_DATA["data"][0]["id"]
-                PLAYER_FIRST_NAME = PLAYER_INFO_DATA["data"][0]["first_name"]
-                PLAYER_LAST_NAME = PLAYER_INFO_DATA["data"][0]["last_name"]
+            if PLAYER_INFO_DATA:
+                PLAYER_FIRST_NAME = PLAYER_INFO_DATA[0].first_name
+                PLAYER_LAST_NAME = PLAYER_INFO_DATA[0].last_name
                 PLAYER_FULL_NAME = PLAYER_FIRST_NAME + " " + PLAYER_LAST_NAME
-                PLAYER_TEAM = PLAYER_INFO_DATA["data"][0]["team"]["full_name"]
-                PLAYER_POSITION = str(PLAYER_INFO_DATA["data"][0]["position"])
-                PLAYER_HEIGHT = str(PLAYER_INFO_DATA["data"][0]["height"])
-                #Handle the situation that we don't get this player's height
-                if "None" in PLAYER_HEIGHT:
-                    PLAYER_HEIGHT = "N/A"
-
-                #Get player's season stats, use the ID we got from above
-                STATS_PARAMS = {
-                    "player_ids[]":PLAYER_ID,
-                    "seasons[]":PLAYER_STATS_SEASON
-                }
-                STATS_RESPONSE = requests.get(url=PLAYER_STATS_URL, headers=headers, params=STATS_PARAMS)
-                print(STATS_RESPONSE.text)
-                STATS_DATA = STATS_RESPONSE.json()
-                PLAYER_PPG = STATS_DATA["data"][0]["pts"]
-                PLAYER_RPG = STATS_DATA["data"][0]["reb"]
-                PLAYER_APG = STATS_DATA["data"][0]["ast"]
+                PLAYER_TEAM = PLAYER_INFO_DATA[0].team.full_name
+                PLAYER_POSITION = PLAYER_INFO_DATA[0].position
+                PLAYER_JERSEY_NUMBER = PLAYER_INFO_DATA[0].jersey_number
+                PLAYER_COUNTRY = PLAYER_INFO_DATA[0].country if "None" not in PLAYER_INFO_DATA[0].country else "N/A"
+                PLAYER_HEIGHT = str(PLAYER_INFO_DATA[0].height) if "None" not in str(PLAYER_INFO_DATA[0].height) else "N/A"
+                PLAYER_WEIGHT = str(PLAYER_INFO_DATA[0].weight) if "None" not in str(PLAYER_INFO_DATA[0].weight) else "N/A"
 
                 #Get player's profile image - skip this part since the API for player's image is deprecated.
                 # IMAGE_RESPONSE = requests.get(url=PLAYER_IMAGE_URL)
@@ -91,34 +69,36 @@ def homepage():
                 #     PLAYER_PHOTO_URL = f"https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{IMAGE_ID}.png"
                 #     return render_template("index.html", form=player_form, name=PLAYER_FULL_NAME, team=PLAYER_TEAM, position=PLAYER_POSITION, height=PLAYER_HEIGHT, ppg=PLAYER_PPG, rpg=PLAYER_RPG, apg=PLAYER_APG, image_url=PLAYER_PHOTO_URL, is_post=True, found=True)
                 #Find the player but did not have image
-                return render_template("index.html", form=player_form, name=PLAYER_FULL_NAME, team=PLAYER_TEAM, position=PLAYER_POSITION, height=PLAYER_HEIGHT, ppg=PLAYER_PPG, rpg=PLAYER_RPG, apg=PLAYER_APG, image_url=UNKNOWN_IMAGE_URL, is_post=True, found=True)
-                # len(PLAYER_INFO_DATA["data"]) == 0:
+                return render_template("index.html", form=player_form, name=PLAYER_FULL_NAME, team=PLAYER_TEAM, position=PLAYER_POSITION, height=PLAYER_HEIGHT, weight=PLAYER_WEIGHT, number=PLAYER_JERSEY_NUMBER, player_country=PLAYER_COUNTRY, image_url=UNKNOWN_IMAGE_URL, is_post=True, found=True)
             else:
                 return render_template("index.html", form=player_form, not_found=True)
         #Something went wrong when parsing or handling the response of API
         except:
             return render_template("exception.html")
 
+    #Get
     return render_template("index.html", form=player_form)
 
 #Team search website
 @app.route("/team", methods=["GET", "POST"])
 def team():
     team_form = TeamForm()
+
+    #Post
     if team_form.validate_on_submit() == True:
         team_input = team_form.team.data
         team_input = team_input.title()
         try:
-            TEAM_RESPONSE = requests.get(url = TEAM_URL, headers=headers)
-            TEAM_DATA_LIST = TEAM_RESPONSE.json()["data"]
-            for team in TEAM_DATA_LIST:
+            teams_response = api.nba.teams.list()
+            TEAMS_INFO_DATA = teams_response.data
+            for team in TEAMS_INFO_DATA:
                 #User can either input heat or miami heat, both will work
-                if team_input == team["full_name"] or team_input == team["name"]:
-                    team_name = team["full_name"]
-                    abbreviation = team["abbreviation"]
-                    conference = team["conference"]
-                    division = team["division"]
-                    image_name = team["name"].lower()
+                if team_input == team.full_name or team_input == team.name:
+                    team_name = team.full_name
+                    abbreviation = team.abbreviation
+                    conference = team.conference
+                    division = team.division
+                    image_name = team.name.lower()
                     #Use the team_logo_dict we have created in teamlogo
                     logo_url = team_logo_dict[image_name]
 
@@ -129,6 +109,7 @@ def team():
         except:
             return render_template("except.html")
 
+    #Get
     return render_template("team.html", form=team_form)
 
 if __name__ == "__main__":
